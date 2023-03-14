670375a3986c527f4af45ec09a59980d30aaaacb
donghee <jjj4435@naver.com>
2023-03-14 14:05:44 +0900

    Initial commit

---
+++ src/main/java/com/codestates/seb/httpstest/HttpstestApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.seb.httpstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpstestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/seb/httpstest/HttpstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.seb.httpstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
